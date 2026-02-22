93d84b9c251b79369d7d6cf7ab10967ce66a161b
Thiw <wiriyaphat6789@gmail.com>
2026-02-22 18:37:00 +0700

    Clear Display ready for in game details

--- Project24Game/UI24.cpp
+++ Project24Game/UI24.cpp
@@ -36,6 +36,12 @@ int main()
     
     sf:: RectangleShape button_24 = button24.builtButton();
     sf:: RectangleShape button_N = buttonN.builtButton();
+
+    // for check mouse is over button or not and click button or not so we process more easily
+    bool isOver_24 = false;
+    bool isOver_N = false;
+    bool isClicked_24 = false;
+    bool isClicked_N = false;
     
     // GameOn
     while (window.isOpen()) // using to make window always open 
@@ -48,14 +54,18 @@ int main()
         if (button_24.getGlobalBounds().contains(mouse_pos)) // check button is coline with mouse : augmentNeedtoCheck.getGlobalBound().contains(whatIscheckwith); >> getGlobalBound() = check coline
         {
             button_24.setFillColor(sf::Color(0,75,22)); // we can create animated or click to process
+            isOver_24 = true;
         }else{
             button_24.setFillColor(button24.ColorBox); // reset went not set
+            isOver_24 = false;
         }
         if (button_N.getGlobalBounds().contains(mouse_pos)) // check button is coline with mouse : augmentNeedtoCheck.getGlobalBound().contains(whatIscheckwith); >> getGlobalBound() = check coline
         {
             button_N.setFillColor(sf::Color(0,75,22)); // we can create animated or click to process
+            isOver_N = true;
         }else{
             button_N.setFillColor(buttonN.ColorBox); // reset went not set
+            isOver_N = false;
         }
 
         
@@ -67,8 +77,28 @@ int main()
         window.draw(button24.txtBox(button24.builtButton().getGeometricCenter()));
         window.draw(buttonN.txtBox(buttonN.builtButton().getGeometricCenter()));    
 
+        //------------------------------------------------------------------
+        if(sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) // clicked 
+        {
+            if(isOver_24){ // clicked happen on button_24 or not 
+                isClicked_24 = true;
+            }
 
-        
+            if(isOver_N){ // clicked happen on button_N or not
+                isClicked_N = true;
+            }
+            
+        }
+        if(isClicked_24) 
+        {
+            window.clear(sf::Color::Black); // Start GAME24
+        }
+
+        if(isClicked_N)
+        {
+            window.clear(sf::Color::Red); // Start RANDOM mode
+        }
+    
         // end the current frame
         window.display();
     }
