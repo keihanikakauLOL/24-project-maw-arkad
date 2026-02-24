5eb86b9a3aa69fba4b4bf1b91e25df0e0f8b2d4c
SolidGA5 <phuphing.gh.chompubang@gmail.com>
2026-02-24 21:58:46 +0700

    #GameSystem 24 can be input number but can't still delete

--- Project24Game/GameUi.h
+++ Project24Game/GameUi.h
@@ -1,11 +1,12 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 string Fontloc = "C:/Windows/Fonts/arial.ttf";
-float windowSize_x = 1600;
-float WindowSize_y = 900;
+float windowSize_x = 800;
+float WindowSize_y = 800;
 sf::Font font(Fontloc); //import font_style variable (pull form location)  
 
 class buttonBuild{
@@ -33,7 +34,9 @@ class Title{
 
 sf::Text Title::TitleName(){
     sf::Text Title_24(font,Title_string,TittleGameSize);
-    Title_24.setPosition({windowSize_x*(2.5/5)-TittleGameSize-50,50});
+    sf::FloatRect boundTitle = Title_24.getLocalBounds();
+    Title_24.setOrigin({boundTitle.position.x+boundTitle.size.x/2,TittleGameSize-boundTitle.position.y});
+    Title_24.setPosition({windowSize_x/2,150});
     return Title_24;
 };
 
@@ -41,17 +44,51 @@ sf::Text Title::TitleName(){
 
 sf::RectangleShape buttonBuild::builtButton(){
     sf::RectangleShape button({buttonSize_x,buttonSize_y}); //Create button variable with size
-    button.setOrigin(button.getGeometricCenter());
     button.setPosition({posBox_x,posBox_y}); 
+    button.setOrigin(button.getGeometricCenter());
     button.setFillColor(ColorBox);
     return button;
 };
 
 sf:: Text buttonBuild::txtBox(sf::Vector2f buttonCenter){
-    sf::Text Text(font,name,FontSize);
-    Text.setOrigin(buttonCenter);
-    Text.setPosition({posBox_x+X,posBox_y+Y});
+    sf::Text Text(font,name,FontSize); 
+    sf::FloatRect bounds = Text.getLocalBounds();
+    Text.setOrigin({bounds.position.x+bounds.size.x/2,FontSize-bounds.position.y});
+    Text.setPosition({posBox_x,posBox_y}); 
     return Text;
 }
+
+class Screen{
+    string strData = "";
+    public:
+        void add(char);
+        sf::Text printData();
+        sf::RectangleShape BoxScreen();
+};
+
+void Screen::add(char inputNumber){
+    strData+=inputNumber;
+}
+
+sf::Text Screen::printData(){
+    sf::Text Data(font,strData,100);
+    sf::FloatRect boundScreen = Data.getLocalBounds();
+    Data.setOrigin({boundScreen.position.x+boundScreen.size.x/2,100-boundScreen.position.y});
+    Data.setPosition({windowSize_x/2,150});
+    return Data;
+}
+
+sf::RectangleShape Screen::BoxScreen(){
+    sf::RectangleShape Box({windowSize_x*80/100,WindowSize_y*20/100}); //Create button variable with size
+    Box.setPosition({windowSize_x/2,150}); 
+    Box.setOrigin(Box.getGeometricCenter());
+    Box.setFillColor(sf::Color(128,128,128));
+    return Box;
+}
+
+
+
+
+
 
 
--- Project24Game/UI24.cpp
+++ Project24Game/UI24.cpp
@@ -1,19 +1,39 @@
 #include "GameUi.h"
 
+void GameSystem24(string);
+void Menu();
+
+sf::RenderWindow window(sf::VideoMode({800,800}), "GAME24"); //Create window variable with size and tittle name'
+
+enum GameState { // state of game to process more easily
+    MENU,
+    GAME24,
+    RANDOM_MODE
+};
+GameState state = MENU;
 
 int main()
-{   
+{    
+    
+    while (window.isOpen())
+    {
+        if (state == MENU){Menu();}
+        if (state == GAME24){GameSystem24("1234");}
+    }
+    
+}
+
+void Menu(){
     // Build Windows Frame
-    sf::RenderWindow window(sf::VideoMode({1600,900}), "GAME24"); //Create window variable with size and tittle name'
+    
     Title tilte;
 
-
     // button24
-    buttonBuild button24 = {
-        .posBox_x = windowSize_x*1/5,
-        .posBox_y = WindowSize_y/(float)2, // 2.5 when no float was assian as double type
-        .FontSize = 75,
-        .buttonSize_x = 300,
+    buttonBuild button24 = buttonBuild{
+        .posBox_x = windowSize_x*1/4,
+        .posBox_y = WindowSize_y/2.f, // 2.5 when no float was assian as double type
+        .FontSize = 50,
+        .buttonSize_x = 225,
         .buttonSize_y = 100,
         .X = 5,
         .Y = 0,
@@ -22,11 +42,11 @@ int main()
     };
 
     // buttonN
-    buttonBuild buttonN = {
-        .posBox_x = windowSize_x*4/5,
-        .posBox_y = WindowSize_y/(float)2, // 2.5 when no float was assian as double type
-        .FontSize = 75,
-        .buttonSize_x = 300,
+    buttonBuild buttonN = buttonBuild{
+        .posBox_x = windowSize_x*3/4,
+        .posBox_y = WindowSize_y/(2.f), // 2.5 when no float was assian as double type
+        .FontSize = 50,
+        .buttonSize_x = 225,
         .buttonSize_y = 100,
         .X = 5,
         .Y = 0,
@@ -41,79 +61,156 @@ int main()
     bool isOver_24 = false;
     bool isOver_N = false;
     
-
-    enum GameState { // state of game to process more easily
-    MENU,
-    GAME24,
-    RANDOM_MODE
-};
-
-GameState state = MENU;
     
-
-
     // GameOn
-    while (window.isOpen()) // using to make window always open 
-    {
+    while (state == MENU) // using to make window always open 
+    {        
+        window.clear();
         while (const std::optional event = window.pollEvent()) // Closing windows functions
         {
-            if (event->is<sf::Event::Closed>()) window.close(); // "close requested" event: we close the window 
-        }// 
-        auto mouse_pos = sf::Vector2f(sf::Mouse::getPosition(window));
-        if (button_24.getGlobalBounds().contains(mouse_pos)) // check button is coline with mouse : augmentNeedtoCheck.getGlobalBound().contains(whatIscheckwith); >> getGlobalBound() = check coline
-        {
-            button_24.setFillColor(sf::Color(0,75,22)); // we can create animated or click to process
-            isOver_24 = true;
-        }else{
-            button_24.setFillColor(button24.ColorBox); // reset went not set
-            isOver_24 = false;
-        }
-        if (button_N.getGlobalBounds().contains(mouse_pos)) // check button is coline with mouse : augmentNeedtoCheck.getGlobalBound().contains(whatIscheckwith); >> getGlobalBound() = check coline
-        {
-            button_N.setFillColor(sf::Color(0,75,22)); // we can create animated or click to process
-            isOver_N = true;
-        }else{
-            button_N.setFillColor(buttonN.ColorBox); // reset went not set
-            isOver_N = false;
-        }
-
-        
-        // windown.draw is order by line to line upper = under
-        window.clear(sf::Color::Black);
-        window.draw(tilte.TitleName());
-        window.draw(button_24);
-        window.draw(button_N);
-        window.draw(button24.txtBox(button24.builtButton().getGeometricCenter()));
-        window.draw(buttonN.txtBox(buttonN.builtButton().getGeometricCenter()));    
-
-        //------------------------------------------------------------------
-        if(sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) // clicked 
-        {   
-            if (state == MENU) // only process when in menu state
+            if (event->is<sf::Event::Closed>()) window.close();// "close requested" event: we close the window 
+            auto mouse_pos = sf::Vector2f(sf::Mouse::getPosition(window));
+            if (button_24.getGlobalBounds().contains(mouse_pos)) // check button is coline with mouse : augmentNeedtoCheck.getGlobalBound().contains(whatIscheckwith); >> getGlobalBound() = check coline
             {
+                button_24.setFillColor(sf::Color(0,75,22)); // we can create animated or click to process
+                isOver_24 = true;
+            }else{
+                button_24.setFillColor(button24.ColorBox); // reset went not set
+                isOver_24 = false;
+            }
+            if (button_N.getGlobalBounds().contains(mouse_pos)) // check button is coline with mouse : augmentNeedtoCheck.getGlobalBound().contains(whatIscheckwith); >> getGlobalBound() = check coline
+            {
+                button_N.setFillColor(sf::Color(0,75,22)); // we can create animated or click to process
+                isOver_N = true;
+            }else{
+                button_N.setFillColor(buttonN.ColorBox); // reset went not set
+                isOver_N = false;
+            }
+
+            if(sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) // clicked 
+            {   
                 if(isOver_24){ // clicked happen on button_24 or not 
                     state = GAME24;
                 }
+            } 
+            // windown.draw is order by line to line upper = under
+            window.draw(tilte.TitleName());
+            window.draw(button_24);
+            window.draw(button_N);
+            window.draw(button24.txtBox(button24.builtButton().getGeometricCenter()));
+            window.draw(buttonN.txtBox(buttonN.builtButton().getGeometricCenter())); 
+            window.display(); 
+        }    
+    }  
+}
 
-                else if(isOver_N){ // clicked happen on button_N or not
-                    state = RANDOM_MODE;
+void GameSystem24(string setNumber){
+    Screen Display;
+    bool gateway[] = {0,0,0,0};
+    buttonBuild number1 = {
+        .posBox_x = windowSize_x*25/100,
+        .posBox_y = WindowSize_y*50/100, // 2.5 when no float was assian as double type
+        .FontSize = 75,
+        .buttonSize_x = 150,
+        .buttonSize_y = 150,
+        .X = 0,
+        .Y = 0,
+        .name = to_string(setNumber[0]-48),
+            .ColorBox = sf::Color(255,20,52)
+        };
+    buttonBuild number2 = {
+            .posBox_x = windowSize_x*50/100,
+            .posBox_y = WindowSize_y*50/100, // 2.5 when no float was assian as double type
+            .FontSize = 75,
+            .buttonSize_x = 150,
+            .buttonSize_y = 150,
+            .X = 0,
+            .Y = 0,
+            .name = to_string(setNumber[1]-48),
+            .ColorBox = sf::Color(255,20,52)
+        };
+    buttonBuild number3 = {
+            .posBox_x = windowSize_x*25/100,
+            .posBox_y = WindowSize_y*75/100, // 2.5 when no float was assian as double type
+            .FontSize = 75,
+            .buttonSize_x = 150,
+            .buttonSize_y = 150,
+            .X = 0,
+            .Y = 0,
+            .name = to_string(setNumber[2]-48),
+            .ColorBox = sf::Color(255,20,52)
+        };
+    buttonBuild number4 = {
+            .posBox_x = windowSize_x*50/100,
+            .posBox_y = WindowSize_y*75/100, // 2.5 when no float was assian as double type
+            .FontSize = 75,
+            .buttonSize_x = 150,
+            .buttonSize_y = 150,
+            .X = 0,
+            .Y = 0,
+            .name = to_string(setNumber[3]-48),
+            .ColorBox = sf::Color(255,20,52)
+        };
+    sf::RectangleShape number_1 = number1.builtButton();
+    sf::RectangleShape number_2 = number2.builtButton();
+    sf::RectangleShape number_3 = number3.builtButton();
+    sf::RectangleShape number_4 = number4.builtButton();
+    while (state == GAME24){
+        window.clear(); 
+        window.draw(number_1);
+        window.draw(number1.txtBox(number_1.getPosition()));
+        window.draw(number_2);
+        window.draw(number2.txtBox(number_2.getPosition()));
+        window.draw(number_3);
+        window.draw(number3.txtBox(number_3.getPosition()));
+        window.draw(number_4);
+        window.draw(number4.txtBox(number_4.getPosition()));
+        while (const std::optional event = window.pollEvent()) // Closing windows functions
+        {
+            if (event->is<sf::Event::Closed>()) window.close();
+            auto mouse_pos = sf::Vector2f(sf::Mouse::getPosition(window));// "close requested" event: we close the window 
+            if (number_1.getGlobalBounds().contains(mouse_pos)) 
+            {
+                number_1.setFillColor(sf::Color(44,75,22)); 
+                if (gateway[0] == 0 && sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)){
+                    gateway[0] = 1;
+                    Display.add(setNumber[0]);
                 }
+            }else{
+                number_1.setFillColor(number1.ColorBox); 
             }
-            
+            if (number_2.getGlobalBounds().contains(mouse_pos))
+            {
+                number_2.setFillColor(sf::Color(44,75,22)); 
+                if (gateway[1] == 0 && sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)){
+                    gateway[1] = 1;
+                    Display.add(setNumber[1]);
+                }
+            }else{
+                number_2.setFillColor(number2.ColorBox);
+            }
+            if (number_3.getGlobalBounds().contains(mouse_pos)) 
+            {
+                number_3.setFillColor(sf::Color(44,75,22)); 
+                if (gateway[2] == 0 && sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)){
+                    gateway[2] = 1;
+                    Display.add(setNumber[2]);
+                }
+            }else{
+                number_3.setFillColor(number3.ColorBox); 
+            }
+            if (number_4.getGlobalBounds().contains(mouse_pos)){
+                number_4.setFillColor(sf::Color(44,75,22));
+                if (gateway[3] == 0 && sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)){
+                    gateway[3] = 1;
+                    Display.add(setNumber[3]);
+                }
+            }else{
+                number_4.setFillColor(number4.ColorBox); 
+            }
         }
-        if(state == GAME24) 
-        {
-            window.clear(sf::Color::Black); // Start GAME24
-           
-        }
-
-        else if(state == RANDOM_MODE)
-        {
-            window.clear(sf::Color::Red); // Start RANDOM mode
-        }
-       
-        // end the current frame
+        window.draw(Display.BoxScreen());
+        window.draw(Display.printData());
         window.display();
-    }
-
+    }   
 }
