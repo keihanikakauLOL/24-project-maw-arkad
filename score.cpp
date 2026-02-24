78b29409abbdf3078a5d5e5d7cce7b669569d5be
pubkung1ez <tirapan_30506@sysp.ac.th>
2026-02-24 22:13:06 +0700

    gamerestart and pause

--- score.cpp
+++ score.cpp
@@ -1,28 +1,121 @@
-
 #include <iostream>
-#include <cstdlib>
+#include <thread>
+#include <chrono>
+#include <atomic>
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
+atomic<int> timeLeft(10);
+atomic<int> timemin(0);
+atomic<bool> running(true);
+atomic<int> score(0);
+atomic<int> streak(1);
+atomic<bool> answeredCorrect(false);
+atomic<int> difficulty(1);
+atomic<int> scoremax(0);
+atomic<bool> runaway(true);
+atomic<bool> restart(false);
+atomic<bool> timeout(true);
 
-    if (argc == 1) {
-        cout << "Please input numbers to find average.\n";
-        return 0;
+void timer_mode_normal() {
+
+    while (runaway) {
+
+        while (running && timeLeft > 0) {
+
+            cout << "\rTime Left: " << timeLeft << " sec" << flush;
+            this_thread::sleep_for(chrono::seconds(1));
+
+            timeLeft--;
+        }
+
+        if (timeout) {
+            cout << "\nTime's up!\n";
+            timeout = false;
+            running = false;
+            streak = 0;
+        }
+
+        this_thread::sleep_for(chrono::milliseconds(50));
+    }
+}
+
+
+void point_mode_normal() {
+
+    while (runaway) {
+
+        if (running && answeredCorrect.load()) {
+
+            int addScore = 10 * difficulty.load() * streak.load();
+            score += addScore;
+
+            if (score.load() > scoremax.load()) {
+                scoremax.store(score.load());
+            }
+
+            answeredCorrect = false;
+            streak++;
+
+            cout << "\nScore: " << score << endl;
+        }
+
+        this_thread::sleep_for(chrono::milliseconds(50));
+    }
+}
+
+
+void game_start() {
+
+    while (runaway) {
+
+        if (restart.load()) {
+
+            score = 0;
+            timeLeft = 10;
+            streak = 1;
+            running = true;
+
+            restart = false;
+
+            cout << "\nGame Restarted!\n";
+        }
+
+        this_thread::sleep_for(chrono::milliseconds(50));
+    }
+}
+
+
+int main() {
+
+    thread t1(timer_mode_normal);
+    thread t2(point_mode_normal);
+    thread t3(game_start);
+
+    string cmd;
+
+    while (true) {
+
+        cin >> cmd;
+
+        if (cmd == "correct") {
+            answeredCorrect = true;
+        }
+        else if (cmd == "restart") {
+            restart = true;
+        }
+        else if (cmd == "quit") {
+            runaway = false;
+            running = false;
+            break;
+        }
     }
 
-    int count = argc - 1;
-    double sum = 0.0;
+    t1.join();
+    t2.join();
+    t3.join();
 
-    for (int i = 1; i < argc; i++) {
-        sum += atof(argv[i]); 
-    }
-
-    double average = sum / count;
-
-    cout << "---------------------------------\n";
-    cout << "Average of " << count << " numbers = " << average << "\n";
-    cout << "---------------------------------\n";
+    cout << "\nFinal Score: " << score << endl;
 
     return 0;
 }
