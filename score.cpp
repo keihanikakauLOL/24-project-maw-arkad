6d21dfeb76fb68a098f38e8aba8268b343639af7
pubkung1ez <tirapan_30506@sysp.ac.th>
2026-02-24 22:09:25 +0700

    gamerestart and pause

--- score.cpp
+++ score.cpp
@@ -1,72 +1,28 @@
+
 #include <iostream>
-#include <thread>
-#include <chrono>
-#include <atomic>
-#include <string>
+#include <cstdlib>
+
 using namespace std;
 
-atomic<int> timeLeft(10);
-atomic<bool> running(true);
-atomic<int> score(0);
-atomic<int> streak(1);
-atomic<bool> answeredCorrect(true);
-atomic<int> difficulty(1);
-atomic<bool> modetimer(true);
-atomic<int> scoremax(0);
+int main(int argc, char* argv[]) {
 
-void timer_mode_normal() {
-    while (running && timeLeft > 0) {
-
-        cout << "\nTime Left: " << timeLeft << " sec" << flush;
-        this_thread::sleep_for(chrono::seconds(1));
-
-        timeLeft--;
+    if (argc == 1) {
+        cout << "Please input numbers to find average.\n";
+        return 0;
     }
 
-    if (timeLeft == 0) {
-        cout << "\nTime's up!\n";
-        running = false;
-        streak = 0;
-    }
-}
+    int count = argc - 1;
+    double sum = 0.0;
 
-void point_mode_normal(){
-    while(answeredCorrect){
-        score += 10*(difficulty/10)*(streak/10);
-        if(score > scoremax){
-            scoremax = score;
-        }
-    }
-    if(!answeredCorrect){
-        answeredCorrect = true;
-        score -= 10*(difficulty/15)*(streak/11.0);
-    }
-}
-
-int main() {
-    if(modetimer){
-    thread t1(timer_mode_normal);
-    thread t2(point_mode_normal);
-    if("ans == incorrect"){
-        answeredCorrect = false;
-    }
-    //
-        //code by arm
-
-
-    //
-    cout << "\nsss\n";
-    t1.join();
-    t2.join();
-    }else{
-    thread t2(point_mode_normal);
-    if("ans == incorrect"){
-        answeredCorrect = false;
+    for (int i = 1; i < argc; i++) {
+        sum += atof(argv[i]); 
     }
 
+    double average = sum / count;
 
-    t2.join();
-    }
+    cout << "---------------------------------\n";
+    cout << "Average of " << count << " numbers = " << average << "\n";
+    cout << "---------------------------------\n";
 
-return 0;
+    return 0;
 }
