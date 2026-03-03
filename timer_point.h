6eeb1691b777d36a8fd25dcc02fde3e5353dcb5c
pubkung1ez <tirapan_30506@sysp.ac.th>
2026-03-03 21:22:44 +0700

    choose choice

--- timer_point.h
+++ timer_point.h
@@ -9,7 +9,7 @@
 
 class Game {
 private:
-    std::atomic<int> timeLeft{20};
+    std::atomic<int> timeLeft{30};
     std::atomic<int> score{0};
     std::atomic<int> streak{1};
     std::atomic<bool> running{true};
@@ -74,7 +74,7 @@ private:
     }
 
     void resetGame() {
-        timeLeft = 20;
+        timeLeft = 30;
         score = 0;
         streak = 1;
         paused = false;
@@ -89,13 +89,24 @@ public:
         t3 = std::thread(&Game::endGame, this);
     }
 
-    void handleCommand(const std::int& cmd) {
+    void choice_func(const std::int& cmd) {
 
-        if (cmd == 0) addPoint();
+        if (cmd == 0) {
+            addPoint();
+            Chooseyourchoicemyfriend();
+        }
         else if (cmd == 1) pauseTimer();
         else if (cmd == 2) resumeTimer();
         else if (cmd == 3) restart();
         else if (cmd == 4) quit();
+    }
+
+    int Chooseyourchoicemyfriend(){
+        running = false;
+        return streak.load();
+        return score.load();
+        return timeLeft.load();
+        timeLeft = 0;
     }
 
     void pauseTimer() {
