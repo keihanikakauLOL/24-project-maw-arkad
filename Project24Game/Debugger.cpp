651594c3977ea0ccb997c73f1aebe68205adccab
KeihanikakauLOL <arm.nantana@gmail.com>
2026-03-09 22:56:41 +0700

    debug

--- Project24Game/Debugger.cpp
+++ Project24Game/Debugger.cpp
@@ -1,13 +1,14 @@
 #include "24GameAlgo.h"
 
 int main(){
-    srand(time(0));createQuestions(); 
-    int n = 100;
-    while (n--){
+    srand(time(0));
+    createQuestions(); 
+    int count = 0;
+    while (true){
+        count++;
         int type_games = rand()%90+10;
         string goalstr = to_string(type_games);
         double goal = (double)type_games;
-        clearShuffvector();
         newCreateList(type_games);
         string setNumber = newGetfile();
         clearSolvector();
@@ -15,10 +16,20 @@ int main(){
         int goal_int = stoi(goalstr);
         for (int i = 0;i<setNumber.size();i++){setNumberDoubleType[i] = (double)(setNumber[i]-48);}
         Solvethegame(setNumberDoubleType,goal_int);
+        if (solutions.size() == 1){
+            cout << "Game Gen : " << goalstr << " !=" << "   Can only Be solve : " << solutions[0] <<"   =>    " << setNumber << endl;
+            return 0;
+        }
         if (goalstr != solutions[0]){
             cout << "Game Gen : " << goalstr << " !=" << "   Can only Be solve : " << solutions[0] <<"   =>    " << setNumber << endl;
             return 0;
         }
-        cout << setNumber << " = " << goal << endl;
+        cout << endl;
+        cout << count << ". " << setNumber << " = " << goal << endl;
+        for(auto const & s : solutions) {
+            cout << s << endl;
+        }
+        cout << endl;
+        cout << " ------------------------------ " << endl;
     }
 }
