ff911464dd047a1500eadd6bfa039bf64ee20615
SolidGA5 <phuphing.gh.chompubang@gmail.com>
2026-03-09 17:38:57 +0700

    #pls helpppppppppp

---
+++ Project24Game/Debugger.cpp
@@ -0,0 +1,24 @@
+#include "24GameAlgo.h"
+
+int main(){
+    srand(time(0));createQuestions(); 
+    int n = 100;
+    while (n--){
+        int type_games = rand()%90+10;
+        string goalstr = to_string(type_games);
+        double goal = (double)type_games;
+        clearShuffvector();
+        newCreateList(type_games);
+        string setNumber = newGetfile();
+        clearSolvector();
+        double setNumberDoubleType[4];
+        int goal_int = stoi(goalstr);
+        for (int i = 0;i<setNumber.size();i++){setNumberDoubleType[i] = (double)(setNumber[i]-48);}
+        Solvethegame(setNumberDoubleType,goal_int);
+        if (goalstr != solutions[0]){
+            cout << solutions[0] << " !=" << goalstr << "   " << setNumber << endl;
+            return 0;
+        }
+        cout << setNumber << " = " << goal << endl;
+    }
+}
